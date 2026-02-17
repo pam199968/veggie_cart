7e25ccfa9220f5fb3341a9285c5c7adafc6786ee
pam199968 <patrick.mortas@gmail.com>
2026-02-17 13:05:01 +0100

    Fix bug "duplicate init of firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ if (keystorePropertiesFile.exists()) {
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
+    //id("com.google.gms.google-services")
     // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
